d0177115f601dc51b5e4bc966ace8879a4156bed
bzr-lipe <bzr.lipe@gmail.com>
2023-02-08 11:01:39 -0300

    accordion ok, mobile and desktop

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
 const btnMobile = document.getElementById('checkbox-menu');   
 
-const headerAccordion1 = document.getElementById('accordion-header1');
+document.getElementById('accordion-header1').onclick=() => {
+    let Accordion = document.getElementById('accordion-item1');
+    let arrowAccordion = document.getElementById('accordion-arrow1');
+    let titleAccordion = document.getElementById('accordion-title1');
 
 
-
-headerAccordion1.onclick=() => {
-    let Accordion1 = document.getElementById('accordion-item1');
-    let arrowAccordion1 = document.getElementById('accordion-arrow1');
-    let titleAccordion1 = document.getElementById('accordion-title1');
-
-
-    if(!Accordion1.classList.contains('accordion-active')){
-        arrowAccordion1.style.transform='rotate(0deg)';
-        Accordion1.classList.add('accordion-active');
-        titleAccordion1.style.color = 'var(--color-main)'
+    if(!Accordion.classList.contains('accordion-active')){
+        arrowAccordion.style.transform='rotate(0deg)';
+        Accordion.classList.add('accordion-active');
+        titleAccordion.style.color = 'var(--color-main)'
     }else{
-        Accordion1.classList.remove('accordion-active');
-        arrowAccordion1.style.transform='rotate(90deg)';
-        titleAccordion1.style.color = 'var(--color-bg-light)'
+        Accordion.classList.remove('accordion-active');
+        arrowAccordion.style.transform='rotate(90deg)';
+        titleAccordion.style.color = 'var(--color-bg-light)'
+    }
+}
+
+document.getElementById('accordion-header2').onclick=() => {
+    let Accordion = document.getElementById('accordion-item2');
+    let arrowAccordion = document.getElementById('accordion-arrow2');
+    let titleAccordion = document.getElementById('accordion-title2');
+
+
+    if(!Accordion.classList.contains('accordion-active')){
+        arrowAccordion.style.transform='rotate(0deg)';
+        Accordion.classList.add('accordion-active');
+        titleAccordion.style.color = 'var(--color-main)'
+    }else{
+        Accordion.classList.remove('accordion-active');
+        arrowAccordion.style.transform='rotate(90deg)';
+        titleAccordion.style.color = 'var(--color-bg-light)'
+    }
+}
+
+document.getElementById('accordion-header3').onclick=() => {
+    let Accordion = document.getElementById('accordion-item3');
+    let arrowAccordion = document.getElementById('accordion-arrow3');
+    let titleAccordion = document.getElementById('accordion-title3');
+
+
+    if(!Accordion.classList.contains('accordion-active')){
+        arrowAccordion.style.transform='rotate(0deg)';
+        Accordion.classList.add('accordion-active');
+        titleAccordion.style.color = 'var(--color-main)'
+    }else{
+        Accordion.classList.remove('accordion-active');
+        arrowAccordion.style.transform='rotate(90deg)';
+        titleAccordion.style.color = 'var(--color-bg-light)'
     }
 }
 
