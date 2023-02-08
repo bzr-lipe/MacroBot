6108b11d42fb983b38980d8b0e90007009f3b133
bzr-lipe <bzr.lipe@gmail.com>
2023-02-08 09:22:34 -0300

    accordion visual ok, implementando funcionalidades e responsividade

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const btnMobile = document.getElementById('checkbox-menu');   
 
-const btnAccordion1 = document.getElementById('accordion-btn1');
-const Accordion1 = document.getElementById('accordion-item1');
+const headerAccordion1 = document.getElementById('accordion-header1');
 
 
-btnAccordion1.onclick=() => {
-    
-    if(Accordion1.style.height = '68px' ){
-        Accordion1.style.height='167px';
+
+headerAccordion1.onclick=() => {
+    let Accordion1 = document.getElementById('accordion-item1');
+    let arrowAccordion1 = document.getElementById('accordion-arrow1');
+    let titleAccordion1 = document.getElementById('accordion-title1');
+
+
+    if(!Accordion1.classList.contains('accordion-active')){
+        arrowAccordion1.style.transform='rotate(0deg)';
+        Accordion1.classList.add('accordion-active');
+        titleAccordion1.style.color = 'var(--color-main)'
     }else{
-        Accordion1.style.height='68px';
+        Accordion1.classList.remove('accordion-active');
+        arrowAccordion1.style.transform='rotate(90deg)';
+        titleAccordion1.style.color = 'var(--color-bg-light)'
     }
 }
 
