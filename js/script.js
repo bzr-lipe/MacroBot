5da9efb2a6a0779d2048f714207d872aaa4d095e
bzr-lipe <bzr.lipe@gmail.com>
2023-02-08 08:29:36 -0300

    Accordion on progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const btnMobile = document.getElementById('checkbox-menu');   
 
+const btnAccordion1 = document.getElementById('accordion-btn1');
+const Accordion1 = document.getElementById('accordion-item1');
 
 
-btnMobile.addEventListener('click', toggleMenu);
+btnAccordion1.onclick=() => {
+    
+    if(Accordion1.style.height = '68px' ){
+        Accordion1.style.height='167px';
+    }else{
+        Accordion1.style.height='68px';
+    }
+}
+
 
